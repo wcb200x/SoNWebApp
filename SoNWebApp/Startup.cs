d094172ca8e56040df6c4555786a1c12453e017d
wcb20 <wcb20@DESKTOP-ERLKTF3>
2017-01-30 16:35:17 -0500

    Add project files.

---
+++ SoNWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoNWebApp.Startup))]
+namespace SoNWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
