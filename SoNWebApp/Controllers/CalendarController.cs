5785c98c9b7fa1b022fe8ccb853d7712a626a365
Taylor Payne <tmpayn03@louisville.edu>
2017-02-04 20:57:16 -0500

    Calendar

---
+++ SoNWebApp/Controllers/CalendarController.cs
@@ -0,0 +1,42 @@
+﻿using DayPilot.Web.Mvc;
+using DayPilot.Web.Mvc.Enums;
+using DayPilot.Web.Mvc.Events.Calendar;
+using SoNWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoNWebApp.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Backend()
+        {
+            return new Dpc().CallBack(this);
+        }
+        class Dpc : DayPilotCalendar
+        {
+            protected override void OnInit(InitArgs e)
+            {
+                
+                var db = new ApplicationDbContext();
+                //Events = from ev in db.events select ev;
+
+                //DataIdField = "id";
+                //DataTextField = "text";
+                //DataStartField = "eventstart";
+                //DataEndField = "eventend";
+
+                //Update();
+            }
+        }
+    }
+}
