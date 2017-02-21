89f4c8bf6a0588b51d83a03d3c7c989641642ef4
Taylor Payne <tmpayn03@louisville.edu>
2017-02-21 17:18:27 -0500

    Logins

--- SoNWebApp/Models/AccountViewModels.cs
+++ SoNWebApp/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace SoNWebApp.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
