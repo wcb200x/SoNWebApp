6aac18c3e9969880088801545fe5c4e3c8ad0251
Taylor Payne <tmpayn03@louisville.edu>
2017-02-24 11:41:24 -0500

    Login Redirects

--- SoNWebApp/Controllers/AccountController.cs
+++ SoNWebApp/Controllers/AccountController.cs
@@ -79,7 +79,29 @@ namespace SoNWebApp.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    var user = await UserManager.FindAsync(model.Email, model.Password);
+                    var roles = await UserManager.GetRolesAsync(user.Id);
+
+                    if (roles.Contains("SuperAdmin"))
+                    {
+                        return RedirectToAction("Home", "Admin");
+                    }
+                    else if (roles.Contains("Admin"))
+                    {
+                        return RedirectToAction("Home", "Admin");
+                    }
+                    else if (roles.Contains("Advisor"))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else if (roles.Contains("Studnet"))
+                    {
+                        return RedirectToAction("StudentDashboard", "Student");
+                    }
+                    else
+                    {
+                        return RedirectToLocal(returnUrl);
+                    }
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
