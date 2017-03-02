03e925a10601a5fabc35267b0eeea29d53ec9254
BasicSetupCommit <wcb200x@gmail.com>
2017-03-02 16:00:37 -0500

    Changes for UserManagement(90% Complete)

---
+++ SoNWebApp/Models/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoNWebApp.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel() { }
+
+        // Allow Initialization with an instance of ApplicationUser:
+        public EditUserViewModel(ApplicationUser user)
+        {
+            UserName = user.UserName;
+            //this.FirstName = user.FirstName;
+            //this.LastName = user.LastName;
+            Email = user.Email;
+        }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
